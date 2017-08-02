a3db1975bdf9b5814595163fa5a14724c0956aaa
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-02 14:41:41 -0500

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,50 @@
    write your logic.
 */
 
+// var hand = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-function handValue (hand) {
+function ace(hand) {
+  var aceHand = [];
+  var noAce = [];
 
-
-  return;
+  for (let j = 0; j < hand.length; j++) {
+    if (hand[j] === "A") {
+      aceHand.push(hand[j]);
+    } else {
+      noAce.push(hand[j]);
+    }
+  }
+  return noAce.concat(aceHand);
 }
+
+
+function handValue(hand) {
+  var valueOfHand = 0;
+
+  hand = ace(hand);
+  for (let i = 0; i < hand.length; i++) {
+    switch (hand[i]) {
+      case "J":
+      case "Q":
+      case "K":
+        valueOfHand += 10;
+        break;
+      case "A":
+        if (valueOfHand + 11 > 21) {
+          valueOfHand = valueOfHand += 1;
+        } else {
+          valueOfHand += 11;
+        }
+        break;
+      default:
+        valueOfHand += Number(hand[i]);
+        break;
+    }
+  }
+  return valueOfHand;
+}
+
+
 
 
 /* -----  Hints ------
